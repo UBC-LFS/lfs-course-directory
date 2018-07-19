4f0f4825788d5c30c4f22f62bf16e45e34fbd6b5
Justin Lee <justinlee0022@gmail.com>
2018-07-19 12:40:48 -0700

    Converted to param

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const getCoursesInTerm = require('./getCoursesInTerm')
-
+const { getYear } = require('./util/get')
+const path = require('path')
+const fs = require('fs-extra')
 const app = express()
 
 app.use((req, res, next) => {
@@ -9,12 +11,18 @@ app.use((req, res, next) => {
   next()
 })
 
-app.get('/W', (req, res) => {
-  getCoursesInTerm('W').then(x => res.send(x))
-})
-
-app.get('/S', (req, res) => {
-  getCoursesInTerm('S').then(x => res.send(x))
+app.get('/:term', async ({ params }, res) => {
+  const term = params
+  const year = getYear()
+  const pathToFile = path.join(__dirname, `courseFiles/${year}${term}`)
+  const exists = await fs.pathExists(pathToFile)
+  if (exists) {
+    const json = await fs.readJson(pathToFile)
+    res.send(json)
+  } else {
+    const courses = await getCoursesInTerm(year, term)
+    await fs.writeJson(pathToFile, courses)
+  }
 })
 
 module.exports = app
